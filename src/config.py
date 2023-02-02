f02bf517eaf4c40f8ebda7fabf9e39d64723b2ea
khiem2105.1 <khiem2105.1@gmail.com>
2023-02-02 18:31:52 +0000

    add config class

---
+++ src/config.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    img_size: int
+    batch_size: int
+    z_size: int
+    gamma_1: float
+    gamma_iters: int
+    gamma_0: float=0
+    img_c: int=3
+    zc: int=1
+    z_std: float=0.1
+    T_iter: int=10
+    T_lr: float=1e-4
+    D_lr: float=1e-4
+    base_factor: int=48
+    plot_interval: int=2000
+    ckpt_interval: int=2000
+    cost: str="weak"
+    max_steps: int=40001
