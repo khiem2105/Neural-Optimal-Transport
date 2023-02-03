2d56b5a96a01f06921b4cb15ac5ada980d77bf3b
khiem2105.1 <khiem2105.1@gmail.com>
2023-02-03 10:51:58 +0000

    fix bug in transform pipeline

--- src/datasets.py
+++ src/datasets.py
@@ -83,7 +83,7 @@ def load_digit_dataset(batch_size: int, root: str, name: str, img_size: int=28, 
         transform = transforms.Compose([
             transforms.Resize([img_size, img_size]),
             transforms.ToTensor(),
-            transform.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
         ])
         
         train_dataset = SVHN(
@@ -102,7 +102,7 @@ def load_digit_dataset(batch_size: int, root: str, name: str, img_size: int=28, 
     elif name == "MNIST":
         transform = transforms.Compose([
             transforms.ToTensor(),
-            transform.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
         ])
 
         train_dataset = MNIST(
