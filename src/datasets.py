5e847a8d616ee90ce90db668a4e9d4b7730231c9
NILS <21207643@ppti-14-502-13.ufr-info-p6.jussieu.fr>
2023-02-11 20:58:34 +0100

    Add flag to enable/disable splitting of celeba dataset into male/female

--- src/datasets.py
+++ src/datasets.py
@@ -20,7 +20,7 @@ class ToyDataset(Dataset):
     def __getitem__(self, index):
         return self.data[index], 0
 
-def load_celeba(img_size, batch_size, root="datasets", num_workers=2, test_ratio=0.1):
+def load_celeba(img_size, batch_size, root="datasets", num_workers=2, test_ratio=0.1, split_male_female=True):
     transform = transforms.Compose([
         transforms.Resize((img_size, img_size)),
         transforms.ToTensor(),
@@ -37,45 +37,64 @@ def load_celeba(img_size, batch_size, root="datasets", num_workers=2, test_ratio
         transform=transform,
         download=True
     )
+    
+    if split_male_female:
+        male_inds = torch.where(celeba.attr[:, 21] == 0)[0]
+        female_inds = torch.where(celeba.attr[:, 21] == 1)[0]
 
-    male_inds = torch.where(celeba.attr[:, 21] == 0)[0]
-    female_inds = torch.where(celeba.attr[:, 21] == 1)[0]
+        male_test_len = int(test_ratio * len(male_inds))
+        male_train_len = len(male_inds) - male_test_len
 
-    male_test_len = int(test_ratio * len(male_inds))
-    male_train_len = len(male_inds) - male_test_len
+        female_test_len = int(test_ratio * len(female_inds))
+        female_train_len = len(female_inds) - female_test_len
 
-    female_test_len = int(test_ratio * len(female_inds))
-    female_train_len = len(female_inds) - female_test_len
+        celeba_male_train, celeba_male_test = random_split(
+            Subset(celeba, male_inds),
+            [male_train_len, male_test_len]
+        )
 
-    celeba_male_train, celeba_male_test = random_split(
-        Subset(celeba, male_inds),
-        [male_train_len, male_test_len]
-    )
+        celeba_female_train, celeba_female_test = random_split(
+            Subset(celeba, female_inds),
+            [female_train_len, female_test_len]
+        )
 
-    celeba_female_train, celeba_female_test = random_split(
-        Subset(celeba, female_inds),
-        [female_train_len, female_test_len]
-    )
+        celeba_male_trainloader = LoaderSampler(
+            DataLoader(celeba_male_train, batch_size=batch_size, num_workers=num_workers, shuffle=True),
+            device=DEVICE
+        )
+        celeba_male_test_loader = LoaderSampler(
+            DataLoader(celeba_male_test, batch_size=batch_size, num_workers=num_workers, shuffle=True),
+            device=DEVICE
+        )
 
-    celeba_male_trainloader = LoaderSampler(
-        DataLoader(celeba_male_train, batch_size=batch_size, num_workers=num_workers, shuffle=True),
-        device=DEVICE
-    )
-    celeba_male_test_loader = LoaderSampler(
-        DataLoader(celeba_male_test, batch_size=batch_size, num_workers=num_workers, shuffle=True),
-        device=DEVICE
-    )
+        celeba_female_trainloader = LoaderSampler(
+            DataLoader(celeba_female_train, batch_size=batch_size, num_workers=num_workers, shuffle=True),
+            device=DEVICE
+        )
+        celeba_female_testloader = LoaderSampler(
+            DataLoader(celeba_female_test, batch_size=batch_size, num_workers=num_workers, shuffle=True),
+            device=DEVICE
+        )
 
-    celeba_female_trainloader = LoaderSampler(
-        DataLoader(celeba_female_train, batch_size=batch_size, num_workers=num_workers, shuffle=True),
-        device=DEVICE
-    )
-    celeba_female_testloader = LoaderSampler(
-        DataLoader(celeba_female_test, batch_size=batch_size, num_workers=num_workers, shuffle=True),
-        device=DEVICE
-    )
-
-    return celeba_male_trainloader, celeba_male_test_loader, celeba_female_trainloader, celeba_female_testloader
+        return celeba_male_trainloader, celeba_male_test_loader, celeba_female_trainloader, celeba_female_testloader
+    else:
+        test_len = int(len(celeba) * test_ratio)
+        train_len = len(celeba) - test_len
+        
+        celeba_train, celeba_test = random_split(celeba, [train_len, test_len])
+        
+        celeba_train_loader = LoaderSampler(
+            DataLoader(celeba_train, batch_size=batch_size, num_workers=num_workers, shuffle=True),
+            device=DEVICE
+        )
+        
+        celeba_test_loader = LoaderSampler(
+            DataLoader(celeba_test, batch_size=batch_size, num_workers=num_workers, shuffle=True),
+            device=DEVICE
+        )
+        
+        return celeba_train_loader, celeba_test_loader
+        
     
 
 def load_digit_dataset(batch_size: int, root: str, name: str, img_size: int=32, num_workers: int=2, duplicate_channels: bool=False):
